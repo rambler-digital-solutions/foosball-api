fefd6e57b770ded403f0d9f47cf3998bd972ba21
Konyukhov Ilia <i.konyukhov@rambler-co.ru>
2016-11-17 13:11:10 +0300

    Updated routes for series

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :rooms
   resources :users
   resources :games
-  resources :invitations
+  resources :series
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   post 'invite', to: :invite, controller: :registration, as: :invite
